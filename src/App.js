510db8d6cef66084e6a6742a0b4991161414abac
Your Name <jbradshaw0526@gmail.com>
2021-09-10 16:00:46 -0500

    Refactored code and added style

--- src/App.js
+++ src/App.js
@@ -1,33 +1,97 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios'
+import styled from 'styled-components';
 import Character from './components/Character'
+import Info from './components/Info';
 
 const App = () => {
-  const [starwarsCharacter, setstarwarsCharacter] = useState([])
+  // const [starwarsCharacter, setstarwarsCharacter] = useState([])
 
-  useEffect(() => {
-    axios.get('https://swapi.dev/api/people/')
-      .then(res => {
-        setstarwarsCharacter(res.data)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }, [])
+  const [characters, setCharacters] = useState(null);
+  const [currentCharacter, setCurrentCharacter] = useState(null);
+  const [error, setError] = useState(null);
 
-  console.table(starwarsCharacter)
-  const Character = starwarsCharacter.map(inner => inner.name + inner.height + inner.mass + inner.hair_color + inner.skin_color)
 
-  console.log(Character)
+  const openInfo = (id) =>
+  {
+      const character = characters.filter(item => item.id === id);
+      setCurrentCharacter(character);
+  };
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-      {Character}
-      
-    </div>
-  );
-}
+  const closeInfo = () =>
+  {
+      setCurrentCharacter(null);
+  };
+
+  useEffect(() =>
+  {
+      console.log("Fetching the characters...");
+      axios.get("https://swapi.dev/api/people")
+          .then(response =>
+          {
+              // save 'response.data' in local variable 'characters'
+              const characters = response.data;
+
+              // add an 'id' to each character on the fly
+              let id = 1;
+              characters.forEach(item => item.id = id++);
+
+              console.log(characters);
+              setCharacters(characters);
+          })
+          .catch(err =>
+          {
+              console.error(err);
+              setError(err);
+          });
+  }, []);
+
+
+  const HeaderContainer = styled.header`
+        color: black;
+        text-shadow: 3px 3px 8px red;
+        text-align: center;
+        font-size: 30px;
+    `;
+
+    const Row = styled.div`
+
+    `;
+
+    const Column = styled.div`
+        flex: 30%;
+    `;
+
+    return (
+        <div className="App">
+            <HeaderContainer>Star Wars Characters</HeaderContainer>
+            {error && <h2>{error}</h2>}
+            <Row>
+                <Column>
+                    {
+                        characters && characters.map(item =>
+                        {
+                            return <Character
+                                key={item.id}
+                                info={item}
+                                openInfo={openInfo}
+                                closeInfo={closeInfo}
+                                currentCharacter={currentCharacter}
+                            />;
+                        })
+                    }
+                </Column>
+                <Column>
+
+                    {
+                        currentCharacter && <Info info={currentCharacter} closeInfo={closeInfo} />
+                    }
+
+                </Column>
+            </Row>
+        </div>
+    );
+}; 
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,40 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Character(props)
+{
+    const Container = styled.div` 
+         width: 50%;
+        display: flex;
+        flex-direction: column;
+        margin: 1% 1% 1% 25%;
+        padding: 8px;
+        background-color: black;
+        opacity: 90%;
+        color: #0077b6;
+
+    span{
+        padding: 10px;
+    }
+`;
+
+const Button = styled.button`
+background: transparent;
+border-radius: 3px;
+border: 2px solid #0077b6;
+color: #0077b6;
+margin: 0.5em 1em;
+padding: 0.25em 1em;
+`;
+
+return (
+<Container>
+    <span>
+        {props.info.name}
+    </span>
+    <Button onClick={() => props.openInfo(props.info.id)}>
+        See Discription
+    </Button>
+</Container>
+);
+} 
